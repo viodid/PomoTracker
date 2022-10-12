9c1a562cb6755c3459de6106e8ea010755949729
viodid <david.yunta.aller@gmail.com>
2022-10-12 22:59:48 +0200

    sign in google

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('logout', views.logout_view, name='logout'),
+    path('privacy', views.privacy, name='privacy'),
+    path('terms', views.terms, name='terms')
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,25 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+from .models import *
 
 
 def index(request):
     return render(request, 'app/index.html')
+
+
+def privacy(request):
+    return render(request, 'app/privacy.html')
+
+
+def terms(request):
+    return render(request, 'app/terms.html')
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
