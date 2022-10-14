34d81b4257d3ce28dc02f940e5dc2e7cd0d578cc
viodid <david.yunta.aller@gmail.com>
2022-10-14 16:32:22 +0200

    authentication system and index styles

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+
+
+class Pomodoro(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='pomodoros')
+    datetime = models.DateTimeField(auto_now=True)
+    tag = models.CharField(max_length=128, blank=True)
+
+    def __str__(self):
+        return f'{self.user}, {self.datetime}, {self.tag}'
