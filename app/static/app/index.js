1b1813f3a6eedc78b4dcc8c2b1f6fb8ea0bcefe4
viodid <david.yunta.aller@gmail.com>
2022-12-07 14:36:34 +0100

    change labels and cleaner start timer js

--- app/static/app/index.js
+++ app/static/app/index.js
@@ -1,10 +1,56 @@
-import { switchDarkMode, changeLightbulb } from "./theme_toggle";
+/* eslint-disable no-multi-spaces */
+/* eslint-disable import/extensions */
+/* eslint-disable no-alert */
+import { switchDarkMode, changeLightbulb } from './theme_toggle.js';
+import * as time from './timer.js';
 
-// measuring durations
-const atStart = performance.now();
+const overlay = document.querySelector('#overlay');
+const minutes = document.querySelector('#minute');
+const seconds = document.querySelector('#second');
+let   focus = true;
+// future fetch settings user
+const focusTime = 1;
+const focusColor = '#f1c232';
+const breakTime = 5;
+const breakColor = 'greenyellow';
+
+function runTimer() {
+  time.showTimer();
+  time.renderTimer(focusTime, focusColor);
+  const atStart = performance.now();
+  time.startTimer('#ding', atStart, focusTime);
+}
+
+function stopTimer() {
+  time.hideTimer();
+  time.stopTimer('#blaublau');
+}
 
 document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#start').addEventListener('click', () => {
+    runTimer();
+  });
 
-  const overlay = document.querySelector('#overlay');
+  document.querySelector('#cancel').addEventListener('click', () => {
+    // eslint-disable-next-line no-restricted-globals
+    if (confirm('Sure?')) {
+      stopTimer();
+    }
+  });
 
-})
+  document.addEventListener('keydown', (event) => {
+    if (event.code === 'Space' && (overlay.style.visibility === 'hidden' || overlay.style.visibility === '')) {
+      runTimer();
+    } else if (event.code === 'Escape' && overlay.style.visibility === 'visible') {
+      // eslint-disable-next-line no-restricted-globals
+      if (confirm('Sure?')) {
+        stopTimer();
+      }
+    }
+  });
+
+  document.querySelector('#slider').addEventListener('click', () => {
+    switchDarkMode();
+    changeLightbulb();
+  });
+});
