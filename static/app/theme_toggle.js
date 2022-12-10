08acca4af6acc64f2dc290cc819f0e350efddfde
viodid <david.yunta.aller@gmail.com>
2022-12-10 14:39:13 +0100

    patch sound start and end timers

--- static/app/theme_toggle.js
+++ static/app/theme_toggle.js
@@ -1,28 +1,27 @@
-export function switch_dark_mode() {
-    document.body.classList.toggle('white-theme');
-    const token = document.querySelector('#token').value
-    let white = document.querySelector('body').classList
-    white.value === 'white-theme' ? white = true : white = false;
+export function switchDarkMode() {
+  document.body.classList.toggle('white-theme');
+  const token = document.querySelector('#token').value;
+  let white = document.querySelector('body').classList;
+  // eslint-disable-next-line no-unused-expressions
+  white.value === 'white-theme' ? white = true : white = false;
 
-
-    fetch(`/api/${token}/settings`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            'white_theme': white
-        })
-    })
+  fetch(`/api/${token}/settings`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      white_theme: white,
+    }),
+  });
 }
 
 // Change image: lightbulb/moon
-export function change_lightbulb() {
-    const lightbulb = document.querySelector('#lightbulb');
-    const moon = document.querySelector('#moon');
-    if (lightbulb.style.display === 'block') {
-        lightbulb.style.display = 'none';
-        moon.style.display = 'block';
-    } else {
-        lightbulb.style.display = 'block';
-        moon.style.display = 'none';
-
-    }
+export function changeLightbulb() {
+  const lightbulb = document.querySelector('#lightbulb');
+  const moon = document.querySelector('#moon');
+  if (lightbulb.style.display === 'block') {
+    lightbulb.style.display = 'none';
+    moon.style.display = 'block';
+  } else {
+    lightbulb.style.display = 'block';
+    moon.style.display = 'none';
+  }
 }
