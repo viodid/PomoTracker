49c7a78f8bdaed3c8cf4e0339f5ee34ff99b2b29
viodid <david.zwsse@simplelogin.co>
2022-12-30 15:28:59 +0100

    tag

--- PomoTracker/__init__.py
+++ PomoTracker/__init__.py
@@ -1,3 +1,11 @@
+import subprocess
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # https://stackoverflow.com/a/46237916/17260275
-__version__ = '1.0.1'
+__version__ = subprocess.check_output(["git", "describe", "--tags", "--always"],
+                                      cwd=BASE_DIR).decode('UTF-8').strip()
 __build__ = ''
+
+print('here', __version__, __build__)
