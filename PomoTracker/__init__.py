922cc5025bf4f4b35265a801b40ff797efc92ba8
viodid <david.zwsse@simplelogin.co>
2023-01-02 13:50:00 +0000

    change __init__ version compatibility

--- PomoTracker/__init__.py
+++ PomoTracker/__init__.py
@@ -4,14 +4,13 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # https://stackoverflow.com/a/46237916/17260275
-version = subprocess.check_output(["git", "describe", "--tags", "--always"],
-                                      cwd=BASE_DIR).decode('UTF-8').strip()
-
-version = version.split('-')
-
 try:
+    version = subprocess.check_output(["git", "describe", "--tags", "--always"],
+                                      cwd=BASE_DIR).decode('UTF-8').strip()
+    version = version.split('-')
+
     __version__ = version[0] + '.' + version[1]
 except:
-    __version__ = version[0]
+    __version__ = '1.2.4'
 
 print(__version__)
