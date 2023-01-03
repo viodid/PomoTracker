c29fa64c2366c14295c47ac7b8e7e8adff9ea408
viodid <david.zwsse@simplelogin.co>
2023-01-03 13:59:20 +0100

    fix version update

--- PomoTracker/__init__.py
+++ PomoTracker/__init__.py
@@ -1,16 +1,11 @@
-import subprocess
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # https://stackoverflow.com/a/46237916/17260275
-try:
-    version = subprocess.check_output(["git", "describe", "--tags", "--always"],
-                                      cwd=BASE_DIR).decode('UTF-8').strip()
-    version = version.split('-')
+file = open(BASE_DIR / "version.txt", "r")
+version = file.read().split('-')
 
-    __version__ = version[0] + '.' + version[1]
-except:
-    __version__ = '1.3.6'
+__version__ = version[0] + '.' + version[1]
 
 print(__version__)
