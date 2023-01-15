1b640286ef37e35f82089d83d97cd2b16be038be
viodid <david.zwsse@simplelogin.co>
2023-01-15 20:38:27 +0100

    enhance leaderboard tabs

--- app/static/app/leaderboard.js
+++ app/static/app/leaderboard.js
@@ -1,13 +1,23 @@
 /* eslint-disable import/extensions */
 import { switchDarkMode, changeLightbulb } from './theme_toggle.js';
+import { whiteTheme } from './user_settings.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  // Denote element by changing its color
-  const path = window.location.pathname.split('/').pop();
-  document.querySelector(`#${path}-leaderboard`).style.color = '#f1c232';
+// Denote element by changing its color
+const path = window.location.pathname.split('/').pop();
+const selection = document.querySelector(`#${path}-leaderboard`);
+selection.style.color = '#f1c232';
+const parent = selection.parentElement;
+console.log(whiteTheme);
+if (!whiteTheme) {
+  parent.style.color = '#efefef';
+  parent.style.backgroundColor = '#202020';
+} else {
+  parent.style.color = '#121212';
+  parent.style.backgroundColor = '#ebebeb';
+}
+parent.style.borderTop = '2px solid #f1c232';
 
-  document.querySelector('#slider').addEventListener('click', () => {
-    switchDarkMode();
-    changeLightbulb();
-  });
+document.querySelector('#slider').addEventListener('click', () => {
+  switchDarkMode();
+  changeLightbulb();
 });
