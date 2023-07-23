d3d457bd71305c8bcc2246d85c8f69974953c9f0
viodid <david.zwsse@simplelogin.co>
2023-07-23 21:34:55 +0200

    update settings.py

--- PomoTracker/settings.py
+++ PomoTracker/settings.py
@@ -19,7 +19,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print('Base dir: ', BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
