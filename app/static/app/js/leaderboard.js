810fb5add2981d1ed4edb83e92e23b8beccb293d
viodid <david.zwsse@simplelogin.co>
2023-08-20 17:12:52 +0200

    finish leaderoard

--- app/static/app/js/leaderboard.js
+++ app/static/app/js/leaderboard.js
@@ -9,13 +9,11 @@ const data = fetch('/api/leaderboard').
 ).then((data) => {
   return data;
 });
-
 // Add event listener to the period buttons
 const radioInputs = document.querySelectorAll('input[type="radio"][name="plan"]');
 radioInputs.forEach(radioInput => {
     radioInput.addEventListener('change', function() {
         if (this.checked) {
-            console.log("Radio option selected:", this.id);
             let selectedPeriod = this.id;
             displayLeaderboard(selectedPeriod, data);
 
@@ -27,13 +25,28 @@ radioInputs.forEach(radioInput => {
 function displayLeaderboard(selectedPeriod, data) {
   deleteLeaderboard();
   data.then((data) => {
-    console.log(data);
+    data = orderData(data, selectedPeriod);
     for (const user of Object.keys(data)) {
       const delay = Object.keys(data).indexOf(user) * 0.1;
       displayItemDelay(user, delay, selectedPeriod, data);
       }
     }
   );
+}
+
+
+function orderData(data, selectedPeriod) {
+  const orderedData = {};
+  const users = Object.keys(data);
+  users.sort((a, b) => {
+    const aCount = getCountPeriod(data[a]['pomos'], selectedPeriod);
+    const bCount = getCountPeriod(data[b]['pomos'], selectedPeriod);
+    return bCount - aCount;
+  });
+  for (const user of users) {
+    orderedData[user] = data[user];
+  }
+  return orderedData;
 }
 
 
@@ -119,26 +132,37 @@ function getCountPeriod(pomodoros, period) {
 
   const startDate = new Date(today); // Default to today
   if (period === 'week') {
-    startDate.setDate(today.getDate() - today.getDay()); // Start of the week (Sunday)
+    // Calculate the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+    const dayOfWeek = currentDate.getDay();
+    // Calculate the number of days to subtract to get to the start of the week (Monday)
+    const daysToSubtract = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
+    // Subtract the days to get to the start of the week
+    startDate.setDate(currentDate.getDate() - daysToSubtract);
   } else if (period === 'month') {
     startDate.setDate(1); // Start of the month
   } else if (period === 'year') {
     startDate.setMonth(0, 1); // Start of the year
+  } else if (period === 'total') {
+    startDate.setFullYear(2020, 1, 1); // Start of the project
   }
 
   const endDate = new Date(today);
-  if (period === 'all') {
-    endDate.setFullYear(2100, 0, 1); // Far future date to include all data
-  }
+
+  endDate.setDate(today.getDate() + 1);
+
+  console.log('startDate', startDate, 'endDate', endDate);
 
   let totalPomodoros = 0;
   for (const date in pomodoros) {
     if (pomodoros.hasOwnProperty(date)) {
       const currentDate = new Date(date);
-      if (currentDate >= startDate && currentDate < endDate) {
+      if (currentDate >= startDate && currentDate <= endDate) {
         totalPomodoros += pomodoros[date];
       }
     }
   }
   return totalPomodoros;
 }
+
+// Set the default period to month when the page loads
+displayLeaderboard('month', data);
